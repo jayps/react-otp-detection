f392ff9919650938e0b588a189c4d8a12bd78218
JP Meyer <jp@jpmeyer.dev>
2023-07-13 08:38:52 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -3,24 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = React.useState('')
+
+    React.useEffect(() => {
+        if ('OTPCredential' in window) {
+            console.log('Setting up OTP listener...');
+            const ac = new AbortController();
+            navigator.credentials.get({
+                otp: {transport: ['sms']},
+                signal: ac.signal
+            }).then(otp => {
+                setValue(otp.code);
+            }).catch(err => {
+                console.log(err);
+            });
+        }
+    }, []);
+
+    const onChange = (e) => {
+        setValue(e.target.value);
+    }
+
+    return (
+        <div className="App">
+            <form>
+                <input autoComplete="one-time-code" required value={value} onChange={onChange}/>
+                <input type="submit"/>
+            </form>
+        </div>
+    );
 }
 
 export default App;
